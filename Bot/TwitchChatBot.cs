cb53f17b2c7c3873bf624868e9e0ce0701b9fd15
Rafał Klinowski <klinowskirafal@gmail.com>
2020-01-05 17:33:17 +0100

    Add project files.

---
+++ Bot/TwitchChatBot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Client.Models;
+
+namespace TwitchCopypastaBot.Bot
+{
+	internal class TwitchChatBot
+	{
+		private readonly ConnectionCredentials credentials = new ConnectionCredentials();
+	}
+}
